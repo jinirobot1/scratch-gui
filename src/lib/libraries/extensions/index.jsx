1e126086116a5299262c7acb7ceda30faa4c7a10
Entry Dev <entrydev@nts-corp.com>
2021-12-31 11:31:30 +0900

    extension define

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -46,7 +46,42 @@ import gdxforInsetIconURL from './gdxfor/gdxfor-small.svg';
 import gdxforConnectionIconURL from './gdxfor/gdxfor-illustration.svg';
 import gdxforConnectionSmallIconURL from './gdxfor/gdxfor-small.svg';
 
+import aibotIconURL from './aibot/aibot.png';
+import aibotInsetIconURL from './aibot/aibot-small.svg';
+import aibotConnectionIconURL from './aibot/aibot-illustration.svg';
+import aibotConnectionSmallIconURL from './aibot/aibot-small.svg';
+
 export default [
+    {
+        name: 'AIBot',
+        extensionId: 'aibot',
+        collaborator: 'JINI',
+        iconURL: aibotIconURL,
+        insetIconURL: aibotInsetIconURL,
+        description: (
+            <FormattedMessage
+                defaultMessage="Connect your projects with the world."
+                description="Description for the 'AIBot' extension"
+                id="gui.extension.aibot.description"
+            />
+        ),
+        featured: true,
+        disabled: false,
+        bluetoothRequired: false,
+        internetConnectionRequired: false,
+        launchPeripheralConnectionFlow: true,
+        useAutoScan: false,
+        connectionIconURL: aibotConnectionIconURL,
+        connectionSmallIconURL: aibotConnectionSmallIconURL,
+        connectingMessage: (
+            <FormattedMessage
+                defaultMessage="Connecting"
+                description="Message to help people connect to their AIBot."
+                id="gui.extension.aibot.connectingMessage"
+            />
+        ),
+        helpLink: 'https://www.jinirobot.com'
+    },
     {
         name: (
             <FormattedMessage
